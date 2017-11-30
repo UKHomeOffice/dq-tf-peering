0af98f969de3b9cab166b0dfb9a7ff20d45e3573
david-wirelab <info@wirelab.co.uk>
2017-11-30 09:55:58 +0000

    added logs s3 endpoint

--- s3.tf
+++ s3.tf
@@ -1,4 +1,9 @@
-resource "aws_vpc_endpoint" "S3Endpoint" {
+resource "aws_vpc_endpoint" "configs3endpoint" {
+  vpc_id       = "${aws_vpc.peeringvpc.id}"
+  service_name = "com.amazonaws.eu-west-2.s3"
+}
+
+resource "aws_vpc_endpoint" "logss3endpoint" {
   vpc_id       = "${aws_vpc.peeringvpc.id}"
   service_name = "com.amazonaws.eu-west-2.s3"
 }
