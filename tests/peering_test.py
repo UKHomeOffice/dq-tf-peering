498af2acb0240fd286f32a0790d99e3341f79371
david-wirelab <info@wirelab.co.uk>
2017-12-11 15:11:24 +0000

    CIDR list

--- tests/peering_test.py
+++ tests/peering_test.py
@@ -14,10 +14,11 @@ class TestE2E(unittest.TestCase):
               skip_get_ec2_platforms = true
             }
 
-            variable "CIDRs" {
-              type = "string"
-              default = "1.1.1.0/24"
-           }
+            variable "SGCIDRs" {
+              type = "list"
+              default = ["1.1.0.0/24", "1.1.0.0/24", "1.1.0.0/24", "1.1.0.0/24"]
+            }
+
 
             module "peering" {
               source = "./mymodule"
@@ -28,21 +29,9 @@ class TestE2E(unittest.TestCase):
 
 
               cidr_block                            = "1.1.0.0/16"
-              connectivity_tester_subnet_cidr_block = "${var.CIDRs}"
-              haproxy_subnet_cidr_block             = "${var.CIDRs}"
-              opssubnet_cidr_block                  = "${var.CIDRs}"
-              acpcicd_vpc_subnet_cidr_block         = "${var.CIDRs}"
-              acpops_vpc_subnet_cidr_block          = "${var.CIDRs}"
-              acpprod_vpc_subnet_cidr_block         = "${var.CIDRs}"
-              acpvpn_vpc_subnet_cidr_block          = "${var.CIDRs}"
-              data_ingest_cidr_block                = "${var.CIDRs}"
-              data_pipe_apps_cidr_block             = "${var.CIDRs}"
-              data_feeds_cidr_block                 = "${var.CIDRs}"
-              public_subnet_cidr_block              = "${var.CIDRs}"
-              dgdb_apps_cidr_block                  = "${var.CIDRs}"
-              mdm_apps_cidr_block                   = "${var.CIDRs}"
-              int_dashboard_cidr_block              = "${var.CIDRs}"
-              ext_dashboard_cidr_block              = "${var.CIDRs}"
+              connectivity_tester_subnet_cidr_block = "1.1.0.0/24"
+              haproxy_subnet_cidr_block             = "1.1.0.0/24"
+              SGCIDRs                               = "${var.SGCIDRs}"
               peering_connectivity_tester_ip        = "1.1.1.1"
               az                                    = "eu-west-2a"
               name_prefix                           = "dq-"
@@ -57,10 +46,10 @@ class TestE2E(unittest.TestCase):
         self.assertEqual(self.result['peering']["aws_vpc.peeringvpc"]["cidr_block"], "1.1.0.0/16")
 
     def test_peering_tester_cidr_block(self):
-        self.assertEqual(self.result['peering']["aws_subnet.connectivity_tester_subnet"]["cidr_block"], "1.1.1.0/24")
+        self.assertEqual(self.result['peering']["aws_subnet.connectivity_tester_subnet"]["cidr_block"], "1.1.0.0/24")
 
     def test_peering_haproxy_cidr_block(self):
-        self.assertEqual(self.result['peering']["aws_subnet.haproxy_subnet"]["cidr_block"], "1.1.1.0/24")
+        self.assertEqual(self.result['peering']["aws_subnet.haproxy_subnet"]["cidr_block"], "1.1.0.0/24")
 
     def test_az_connectivity_tester(self):
         self.assertEqual(self.result['peering']["aws_subnet.connectivity_tester_subnet"]["availability_zone"], "eu-west-2a")
