5fb057527ccdbffa50b1a40786e8b29bd0e3b3a8
david-wirelab <info@wirelab.co.uk>
2017-12-13 10:06:29 +0000

    added cidr and vpc endpoint outputs

--- output.tf
+++ output.tf
@@ -1,3 +1,15 @@
 output "peeringvpc_id" {
   value = "${aws_vpc.peeringvpc.id}"
 }
+
+output "peeringvpc_cidr_block" {
+  value = "${var.cidr_block}"
+}
+
+output "configs3endpoint_id" {
+  value = "${aws_vpc_endpoint.configs3endpoint.id}"
+}
+
+output "logs3endpoint_id" {
+  value = "${aws_vpc_endpoint.logss3endpoint.id}"
+}
