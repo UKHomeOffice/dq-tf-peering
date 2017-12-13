415e22f42432bbc4eea763aed00fc2d9f1a85592
david-wirelab <info@wirelab.co.uk>
2017-12-13 10:09:34 +0000

    remove se endpoint output - not needed

--- output.tf
+++ output.tf
@@ -5,11 +5,3 @@ output "peeringvpc_id" {
 output "peeringvpc_cidr_block" {
   value = "${var.cidr_block}"
 }
-
-output "configs3endpoint_id" {
-  value = "${aws_vpc_endpoint.configs3endpoint.id}"
-}
-
-output "logs3endpoint_id" {
-  value = "${aws_vpc_endpoint.logss3endpoint.id}"
-}
