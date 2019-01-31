928b22a04b33192a322783ba004381a542258a66
Duncan Mountford <duncan9562@gmail.com>
2019-01-31 13:14:09 +0000

    Add variable and remove output

--- instances.tf
+++ instances.tf
@@ -4,6 +4,7 @@ module "haproxy_instance" {
   route_table_id            = "${aws_route_table.peering_route_table.id}"
   haproxy_subnet_cidr_block = "${var.haproxy_subnet_cidr_block}"
   haproxy_private_ip        = "${var.haproxy_private_ip}"
+  haproxy_private_ip2       = "${var.haproxy_private_ip2}"
   s3_bucket_name            = "${var.s3_bucket_name}"
   s3_bucket_acl             = "${var.s3_bucket_acl}"
   log_archive_s3_bucket     = "${var.log_archive_s3_bucket}"
