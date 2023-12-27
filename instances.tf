ea0ffffed9aa3fdf53e46f39fc31ed20e5fc2300
Ian Relf <ian.relf@digital.homeoffice.gov.uk>
2023-12-27 13:23:35 +0000

    Reference feature branch of haproxy repo - migrated to TF0.13

--- instances.tf
+++ instances.tf
@@ -1,5 +1,5 @@
 module "haproxy_instance" {
-  source                    = "github.com/UKHomeOffice/dq-tf-peering-haproxy"
+  source                    = "github.com/UKHomeOffice/dq-tf-peering-haproxy?ref=yel-8750-migrate-tf-version"
   peeringvpc_id             = aws_vpc.peeringvpc.id
   route_table_id            = aws_route_table.peering_route_table.id
   haproxy_subnet_cidr_block = var.haproxy_subnet_cidr_block
